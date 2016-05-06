155c0eb501b251c25c84f5b3a47e44d2bf441cc4
Farid Hajnal <faridhajnal14@hotmail.com>
2016-05-06 17:36:12 +0200

    Initialize repo

---
+++ server.js
@@ -0,0 +1,50 @@
+var express = require('express');
+
+var app = express();
+var port = 3000;
+
+
+var middleware = {
+    
+    requireAuthentication: function(req,res,next){
+        //adding middleware to express...
+        console.log('private route hit!');
+        next();
+    },
+    
+    logger : function(req,res,next){
+        
+        console.log('REQUEST: ' + req.method+' ' + req.originalUrl +' DATE: ' + new Date().toString());
+        next();
+        
+    }
+    
+    
+};
+
+app.use(middleware.logger); //important to specify it here   
+
+/*app.get('/', function(req,res){
+    
+    res.send('Hello Express');
+    
+});*/
+
+app.get('/route', middleware.requireAuthentication, function(req,res){
+    
+    res.send('Hello Express from route');
+    
+});
+
+
+app.use(express.static(__dirname + '/public'));
+
+console.log(__dirname); //provided by node; index.html is default is non is specified on route (/)
+
+
+app.listen(port, function(){
+    
+    console.log('running on port ' + port);
+
+    
+});
